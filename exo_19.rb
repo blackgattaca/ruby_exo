f86d7630f41a9b4fe60f637787350f44a4b442a5
jhon-ran <jhonnatan.rangel@gmail.com>
2020-04-07 21:30:40 +0200

    exo19

---
+++ exo_19.rb
@@ -0,0 +1,13 @@
+#Programme reprendre l'array des emails créés, et n'affiche que les emails avec un nombre pair.
+
+
+nb = 01
+
+list = while (nb <= 50)
+    if nb % 2 == 0
+        puts (" \"jean.dupont.#{nb}@email.fr\"");
+  
+    end
+nb += 1
+end
+
