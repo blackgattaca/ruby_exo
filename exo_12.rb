5a250e6990cfec7155dd1512f7a0fafea5093b41
jhon-ran <jhonnatan.rangel@gmail.com>
2020-04-07 17:04:04 +0200

    exo12 (caisse tête

---
+++ exo_12.rb
@@ -0,0 +1,14 @@
+# programme qui demande un nombre à l'utilisateur, 
+#puis qui compte jusqu'à ce nombre.
+
+puts "Donne-moi un nombre :"
+#Get chomp prend la réponse de l'utilisateur. Integer permet de caster cette réponse à Int
+#Variable de fois de répetition
+count = gets.chomp.to_i
+
+
+
+count.times do |i|
+    puts "count: #{i}"
+    
+end
