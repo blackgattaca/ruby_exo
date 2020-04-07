198375c21091eb47ac92a8188a1a3fb14b5279e4
blackgattaca <vanessa.tr@hotmail.fr>
2020-04-07 15:34:44 +0200

    exo 9

---
+++ exo_09.rb
@@ -0,0 +1,6 @@
+puts "Bonjour, quel est ton prénom ?"
+user_name = gets.chomp
+puts "Et quel est ton nom de famille ?"
+user_lastname = gets.chomp
+print "Bonjour, "
+puts user_name + " " + user_lastname + " !"
