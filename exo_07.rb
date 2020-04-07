72517cee5dd19d49ce0f46e124f5eeceb24bbac2
blackgattaca <vanessa.tr@hotmail.fr>
2020-04-07 15:29:59 +0200

    exo 7

---
+++ exo_07.rb
@@ -0,0 +1,22 @@
+puts "Bonjour, c'est quoi ton blase ?"
+user_name = gets.chomp
+puts user_name
+
+puts "Bonjour, c'est quoi ton blase ?"
+print "> "
+user_name = gets.chomp
+puts user_name
+
+
+user_name = gets.chomp
+puts user_name
+
+
+
+
+#gets.chomp attend qu'un réponse soit écrite, ça permet de laisser l'utiliseur répondre
+#les différents entre les 3 programmes sont :
+#au 1er le champ est laissé à l'utilisateur apres avoir lu la question,
+#au 2eme la question est toujours posée mais un symbole permet d'indiquer plus facilement qu'il y a un champ d'écriture
+#au 3eme le champs d'écriture est posé directement sans question ni indication
+#les 3 programmes permettent de donner un information puis de la repeter
